dfb1f3756b0c1da38570bb1adbf7bce38cb55917
Bhaswanth Isani <bhaswanthisani@gmail.com>
2023-02-26 09:37:45 +0530

    Added Socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { app } from './libs/server'
+import { Server } from 'socket.io'
+import http from 'http'
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', socket => {
+  console.log(socket.id)
+})
 
 const PORT = process.env.PORT === undefined ? 4000 : process.env.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
